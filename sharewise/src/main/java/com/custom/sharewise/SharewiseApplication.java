f8926d328ce1c7313d625c89f23a7e3b903c9145
Abhijeet <srivastava.abhijeet96@gmail.com>
2024-05-20 20:26:57 +1000

    Initial Commit

---
+++ sharewise/src/main/java/com/custom/sharewise/SharewiseApplication.java
@@ -0,0 +1,13 @@
+package com.custom.sharewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharewiseApplication.class, args);
+	}
+
+}
